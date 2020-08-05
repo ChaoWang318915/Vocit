f9d14ba0b507cba6616d62b07d3f2843d3c00501
Redstar <42382875+ChaoWang318915@users.noreply.github.com>
2020-08-05 10:50:37 -0400

    Revert "Add Space for testing."
    This reverts commit 14370a4559c348d6d3cc7c8e988253a6538750dc.

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\API\PostController;
 use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
- 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
