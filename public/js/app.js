59cda755fa2206aae93a2b16da764e00d4b86e83
Redstar <42382875+ChaoWang318915@users.noreply.github.com>
2020-12-21 09:38:53 +0800

    Facebook share title-1

--- public/js/app.js
+++ public/js/app.js
@@ -3431,11 +3431,19 @@ vue__WEBPACK_IMPORTED_MODULE_2___default.a.use(vue_toast_notification__WEBPACK_I
     openShareDialog: function openShareDialog(tmpId, fb_image) {
       var parent = this;
       FB.ui({
-        method: 'share',
-        href: fb_image,
-        caption: 'vocit.io',
-        description: "Description Section",
-        message: "Message Section"
+        // method: 'share',
+        // href: fb_image,                                                        
+        method: 'share_open_graph',
+        action_type: 'og.shares',
+        display: 'popup',
+        action_properties: JSON.stringify({
+          object: {
+            'og:url': 'vocit.io',
+            'og:title': 'Title to show',
+            'og:description': 'The description',
+            'og:image': fb_image
+          }
+        })
       }, function (response) {
         if (response && !response.error_message) {
           setTimeout( /*#__PURE__*/_asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee() {
