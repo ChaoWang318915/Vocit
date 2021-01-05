d7e930bc4273f571f94b2b49ef41b4722f28beb3
Redstar <42382875+ChaoWang318915@users.noreply.github.com>
2021-01-05 22:18:51 +0800

    Facebook large-image  - 1

--- resources/views/view-exchange.blade.php
+++ resources/views/view-exchange.blade.php
@@ -4,8 +4,8 @@
     <meta property="og:title" content="{!! str_replace('Get', $post->parent_business_name.' received ', $post->parent_short_description )!!}" />
     <meta property="og:description" content="You can also get yours" />
     <meta property="og:type" content="image/jpeg">
-    <meta property="og:image:width" content="500"/>
-    <meta property="og:image:height" content="300"/>
+    <!-- <meta property="og:image:width" content="500"/>
+    <meta property="og:image:height" content="300"/> -->
     <meta property="og:url" content="{{$shareurl}}" /> 
     <meta property="og:image" content="{{$post->facebook_url ? $post->facebook_url : $post->lg_url}}" />
 @endsection
