e6934b5e52dd6672951b21d04822d230137054d7
zhangling0528 <htk052894@outlook.com>
2021-01-08 07:58:52 -0500

    issue manage test for fb post

--- resources/views/facebook-post.blade.php
+++ resources/views/facebook-post.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 @section('seo')
-    <meta property="og:title" content="I got free connection" />
-    <meta property="og:description" content="You can also get yours" />
-    <!-- <meta property="og:type" content="image/jpeg"> -->
-    <!-- <meta property="og:image:width" content="500"/>
-    <meta property="og:image:height" content="300"/> -->
-    <meta property="og:url" content="https://vocit.io/post/251" /> 
-    <meta property="og:image" content="https://vocit.io/post/251" />
+<meta property="og:title" content="Test Title" />
+    <meta property="og:description" content="Test Description" />
+    <meta property="og:type" content="image/jpeg">
+    <meta property="og:image:width" content="800"/>
+    <meta property="og:image:height" content="600"/>
+    <meta property="og:url" content="https://vocit.io/facebookpost" />
+    <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610097280/conference-room-768441_1920.jpg" />
 @endsection
 @section('content')
     <facebook-post-component></facebook-post-component>
