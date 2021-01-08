05427e97bc2a98267dae9d29ebb4a48e54571856
zhangling0528 <htk052894@outlook.com>
2021-01-08 10:14:40 -0500

    fb test #5

--- resources/views/facebook-post.blade.php
+++ resources/views/facebook-post.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.facebook')
 @section('seo')
-    <meta property="og:type" content="image/jpeg">
+    <meta property="og:title" content="Facebook Post Title" />
     <meta property="og:url" content="https://vocit.io/facebookpost" />
     <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610097280/conference-room-768441_1920.jpg" />
 @endsection
