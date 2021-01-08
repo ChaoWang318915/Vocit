2e7eb3f06ae41a5fbd8d193cefa4fc96aeffad95
zhangling0528 <htk052894@outlook.com>
2021-01-08 08:56:49 -0500

    meta data update for url.

--- resources/views/facebook-post.blade.php
+++ resources/views/facebook-post.blade.php
@@ -6,7 +6,7 @@
     <meta property="og:type" content="image/jpeg">
     <!-- <meta property="og:image:width" content="800"/> -->
     <!-- <meta property="og:image:height" content="600"/> -->
-    <meta property="og:url" content="https://vocit.io/facebookpost" />
+    <meta property="og:url" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610097280/conference-room-768441_1920.jpg" />
     <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610097280/conference-room-768441_1920.jpg" />
 @endsection
 @section('content')
