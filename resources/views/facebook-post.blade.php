1f711618336c86cc24f8d60502b7d2d556582363
zhangling0528 <htk052894@outlook.com>
2021-01-08 10:57:41 -0500

    image size updated.

--- resources/views/facebook-post.blade.php
+++ resources/views/facebook-post.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.facebook')
 @section('seo')
     <meta property="og:title" content="Facebook Post Title" />
-    <meta property="og:type" content="website">
+    <meta property="og:type" content="image/jpeg">
+    <meta property="og:image:width" content="1250"/>
+    <meta property="og:image:height" content="983"/>
     <meta property="og:url" content="https://vocit.io/facebookpost" />
     <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610119245/mobile-phone-1917737_1920.jpg" />
 @endsection
