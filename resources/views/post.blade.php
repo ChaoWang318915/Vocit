560dc1ed208c03b0cc9fbfd81199ffed981c435c
zhangling0528 <htk052894@outlook.com>
2021-01-08 08:06:48 -0500

    fb test#1

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,8 +8,10 @@
     <meta property="og:type" content="image/jpeg">
     <meta property="og:image:width" content="800"/>
     <meta property="og:image:height" content="600"/>
-    <meta property="og:url" content="{{$shareurl}}" />
-    <meta property="og:image" content="{{$post->facebook_url ? $post->facebook_url : $post->lg_url}}" />
+    <meta property="og:url" content="https://vocit.io/facebookpost" />
+    <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610097280/conference-room-768441_1920.jpg" />
+    <!-- <meta property="og:url" content="{{$shareurl}}" /> -->
+    <!-- <meta property="og:image" content="{{$post->facebook_url ? $post->facebook_url : $post->lg_url}}" /> -->
 @endsection
 @section('content')
     <div class="ui container post-details" id="stickyContainer">
