c85851a7c61abc7b2c83b056ab44fa52ce1c7766
zhangling0528 <htk052894@outlook.com>
2021-01-08 09:55:45 -0500

    facebook layout added

--- resources/views/facebook-post.blade.php
+++ resources/views/facebook-post.blade.php
@@ -1,13 +1,6 @@
-<!-- @extends('layouts.app') -->
+@extends('layouts.facebook')
 @section('seo')
-    <meta name="viewport" content="width=device-width, minimum-scale=0.1">
-    <meta property="og:title" />
-    <meta property="og:description" />
-    <meta property="og:type" content="image/jpeg">
-    <!-- <meta property="og:image:width" content="800"/> -->
-    <!-- <meta property="og:image:height" content="600"/> -->
-    <meta property="og:url" content="https://vocit.io/facebookpost" />
-    <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610097280/conference-room-768441_1920.jpg" />
+    
 @endsection
 @section('content')
     <facebook-post-component></facebook-post-component>
---
+++ resources/views/layouts/facebook.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+@yield('seo')
+
+<!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/semantic/semantic.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/static-style.less') }}" rel="stylesheet/less" type="text/css">
+    <link href="{{ asset('assets/css/animate.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/custom/custom.css') }}" rel="stylesheet">
+    <script src="{{ asset('assets/js/jquery-3.4.1.min.js') }}"></script>
+</head>
+<body>
+    <div id="app">
+        @yield('content')
+    </div>
+<script src="{{ asset('assets/js/less.js') }}" defer></script>
+<script src="{{ asset('assets/semantic/semantic.js') }}" defer></script>
+<script src="{{ asset('assets/js/scripts.js') }}" defer></script>
+</body>
+</html>
