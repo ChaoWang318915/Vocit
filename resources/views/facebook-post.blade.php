65c2978397ffe7b2290cbffedf2a94fa98d3a417
zhangling0528 <htk052894@outlook.com>
2021-01-08 10:08:18 -0500

    add meta data image url

--- resources/views/facebook-post.blade.php
+++ resources/views/facebook-post.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.facebook')
 @section('seo')
-    
+    <meta property="og:type" content="image/jpeg">
+    <meta property="og:url" content="https://vocit.io/facebookpost" />
+    <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610097280/conference-room-768441_1920.jpg" />
 @endsection
 @section('content')
     <facebook-post-component></facebook-post-component>
