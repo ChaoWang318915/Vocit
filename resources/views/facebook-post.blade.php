0bc3e6e3c96d6fd74da2fa1fe3087060b23be226
zhangling0528 <htk052894@outlook.com>
2021-01-08 10:22:54 -0500

    image url updated

--- resources/views/facebook-post.blade.php
+++ resources/views/facebook-post.blade.php
@@ -2,7 +2,7 @@
 @section('seo')
     <meta property="og:title" content="Facebook Post Title" />
     <meta property="og:url" content="https://vocit.io/facebookpost" />
-    <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610097280/conference-room-768441_1920.jpg" />
+    <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610119245/mobile-phone-1917737_1920.jpg" />
 @endsection
 @section('content')
     <facebook-post-component></facebook-post-component>
