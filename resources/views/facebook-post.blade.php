d82d0f4026d936b67c4585b79bce10b485daf1f5
zhangling0528 <htk052894@outlook.com>
2021-01-08 10:51:16 -0500

    og type updated.

--- resources/views/facebook-post.blade.php
+++ resources/views/facebook-post.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.facebook')
 @section('seo')
     <meta property="og:title" content="Facebook Post Title" />
+    <meta property="og:type" content="website">
     <meta property="og:url" content="https://vocit.io/facebookpost" />
     <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610119245/mobile-phone-1917737_1920.jpg" />
 @endsection
