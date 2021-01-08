ec9b6f039728898db54dd8c88a1e0f3ec2d532c6
zhangling0528 <htk052894@outlook.com>
2021-01-08 09:02:41 -0500

    title description update.

--- resources/views/facebook-post.blade.php
+++ resources/views/facebook-post.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 @section('seo')
     <meta name="viewport" content="width=device-width, minimum-scale=0.1">
-    <meta property="og:title" content="Test Title" />
-    <meta property="og:description" content="Test Description" />
+    <meta property="og:title" content="" />
+    <meta property="og:description" content="" />
     <meta property="og:type" content="image/jpeg">
     <!-- <meta property="og:image:width" content="800"/> -->
     <!-- <meta property="og:image:height" content="600"/> -->
-    <meta property="og:url" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610097280/conference-room-768441_1920.jpg" />
+    <meta property="og:url" content="https://vocit.io/facebookpost" />
     <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610097280/conference-room-768441_1920.jpg" />
 @endsection
 @section('content')
