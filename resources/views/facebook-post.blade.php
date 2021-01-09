d635f6cfb875ed7041710719236825f395fc62d7
zhangling0528 <htk052894@outlook.com>
2021-01-09 04:24:50 -0500

    Revert "FB image test"
    This reverts commit 849e821b0c35322ca91e681905467e418c4631b7.

--- resources/views/facebook-post.blade.php
+++ resources/views/facebook-post.blade.php
@@ -1,12 +1,10 @@
 @extends('layouts.facebook')
 @section('seo')
-    <meta property="og:title" content="FB Title" />
-    <meta property="og:description" content="FB description" />
     <meta property="og:type" content="image/jpeg">
     <meta property="og:image:width" content="1250"/>
     <meta property="og:image:height" content="983"/>
     <meta property="og:url" content="https://vocit.io/facebookpost" />
 @endsection
 @section('content')
-    <img src="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610119245/mobile-phone-1917737_1920.jpg" />
+    <facebook-post-component></facebook-post-component>
 @endsection
