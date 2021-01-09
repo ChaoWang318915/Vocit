485c6a4ed71e94bb705302c47699d36314cee68a
zhangling0528 <htk052894@outlook.com>
2021-01-09 17:53:47 -0500

    add script in the body tag.

--- resources/views/layouts/facebook.blade.php
+++ resources/views/layouts/facebook.blade.php
@@ -1,11 +1,11 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">    
 </head>
 <body>
     @yield('content')
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>
