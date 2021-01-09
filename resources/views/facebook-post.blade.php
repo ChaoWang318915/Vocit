3842d96184cf6618087d2db33a7fae1acdc33307
zhangling0528 <htk052894@outlook.com>
2021-01-09 14:34:03 -0500

    meta data type updated.

--- resources/views/facebook-post.blade.php
+++ resources/views/facebook-post.blade.php
@@ -2,6 +2,7 @@
 @section('seo')
     <meta property="og:site_name" content="Vocit" />
     <meta property="og:title" content="FB title" />
+    <meta property="og:type" content="website">
     <meta property="og:description" content="FB description" />            
     <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610219675/capture.png" />
     <meta property="og:image:secure_url" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610219675/capture.png" />
