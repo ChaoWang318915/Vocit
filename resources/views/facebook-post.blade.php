fb26dbcd4a6917667db0e31974ff98db0c27c375
zhangling0528 <htk052894@outlook.com>
2021-01-09 07:29:48 -0500

    updated meta data.

--- resources/views/facebook-post.blade.php
+++ resources/views/facebook-post.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.facebook')
 @section('seo')
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Vocit" />
     <meta property="og:title" content="FB test title" />
-    <meta property="og:description" content="FB test description" />
-    <meta property="og:type" content="image/jpeg">
+    <meta property="og:description" content="FB test description" />            
+    <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610119245/mobile-phone-1917737_1920.jpg" />
+    <meta property="og:image:secure_url" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610119245/mobile-phone-1917737_1920.jpg" />
     <meta property="og:image:width" content="1250"/>
-    <meta property="og:image:height" content="1250"/>
+    <meta property="og:image:height" content="983"/>
     <meta property="og:url" content="https://vocit.io/facebookpost" />
-    <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610097280/conference-room-768441_1920.jpg" />
 @endsection
 @section('content')
-    <img src="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610097280/conference-room-768441_1920.jpg" />
+    <img src="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610119245/mobile-phone-1917737_1920.jpg" />
 @endsection
