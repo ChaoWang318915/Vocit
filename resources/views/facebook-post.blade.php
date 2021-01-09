17e698b23db6564890ac1d34a629e8e03e7cafb1
zhangling0528 <htk052894@outlook.com>
2021-01-09 14:19:08 -0500

    test image updated.

--- resources/views/facebook-post.blade.php
+++ resources/views/facebook-post.blade.php
@@ -3,12 +3,10 @@
     <meta property="og:site_name" content="Vocit" />
     <meta property="og:title" content="FB title" />
     <meta property="og:description" content="FB description" />            
-    <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610119245/mobile-phone-1917737_1920.jpg" />
-    <meta property="og:image:secure_url" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610119245/mobile-phone-1917737_1920.jpg" />
-    <meta property="og:image:width" content="1250"/>
-    <meta property="og:image:height" content="1250"/>
+    <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610219675/capture.png" />
+    <meta property="og:image:secure_url" content="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610219675/capture.png" />
     <meta property="og:url" content="https://vocit.io/facebookpost" />
 @endsection
 @section('content')
-    <img src="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610119245/mobile-phone-1917737_1920.jpg" />
+    <img src="https://s3.us-east-2.amazonaws.com/cdn.vocit/facebook/1610219675/capture.png" />
 @endsection
